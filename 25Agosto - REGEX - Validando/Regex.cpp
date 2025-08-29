d20105115f12ae62ae057910501d3dda0fc7136c
Ricardo Sanchez <ricardoarath@gmail.com>
2025-08-29 13:55:11 -0600

    Ajustes del patron del regex

--- 25Agosto - REGEX - Validando/Regex.cpp
+++ 25Agosto - REGEX - Validando/Regex.cpp
@@ -7,7 +7,7 @@ int main() {
     std::regex patron_email(R"(^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$)");
 
     std::string email;
-    std::cout << "Introduce un correo electrónico: ";
+    std::cout << "Introduce un correo electronico: ";
     std::getline(std::cin, email);
 
     // Verificamos si el correo cumple con el patrón
